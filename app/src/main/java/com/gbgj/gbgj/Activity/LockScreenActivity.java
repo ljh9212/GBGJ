3df8e3f2341400fe164208b7054ecd29acdca66a
JeonghoonLEE <wjdgns9212@gmail.com>
2018-06-06 11:46:45 +0900

    - 답 recyclerview 추가

--- app/src/main/java/com/gbgj/gbgj/Activity/LockScreenActivity.java
+++ app/src/main/java/com/gbgj/gbgj/Activity/LockScreenActivity.java
@@ -12,7 +12,10 @@ import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.gbgj.gbgj.Adapter.AnswerAdapter;
 import com.gbgj.gbgj.R;
+
+import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -20,15 +23,39 @@ import butterknife.OnClick;
 import vinos.mathjaxwebview.view.MathJaxWebView;
 
 public class LockScreenActivity extends AppCompatActivity {
-    @BindView(R.id.mathVebView_lockscreen) MathJaxWebView mathVebView_lockscreen;
-    @BindView(R.id.fab_lockscreen_call) FloatingActionButton fab_lockscreen_call;
-    @BindView(R.id.fab_lockscreen_unlock) FloatingActionButton fab_lockscreen_unlock;
-    @BindView(R.id.fab_lockscreen_mother) FloatingActionButton fab_lockscreen_mother;
-    @BindView(R.id.recyclerview_lockscreen_answer) RecyclerView recyclerview_lockscreen_answer;
+    @BindView(R.id.mathVebView_lockscreen)
+    MathJaxWebView mathVebView_lockscreen;
+    @BindView(R.id.fab_lockscreen_call)
+    FloatingActionButton fab_lockscreen_call;
+    @BindView(R.id.fab_lockscreen_unlock)
+    FloatingActionButton fab_lockscreen_unlock;
+    @BindView(R.id.fab_lockscreen_mother)
+    FloatingActionButton fab_lockscreen_mother;
+    @BindView(R.id.recyclerview_lockscreen_answer)
+    RecyclerView recyclerview_lockscreen_answer;
 
-protected AnswerAdapter answerAdapter;
+    protected AnswerAdapter mAnswerAdapter;
+    protected LinearLayoutManager mLayoutManager;
 
     static public final String TAG = "LockScreenActivity";
+
+    @OnClick(R.id.fab_lockscreen_call)
+    public void onLockScreenCallClick() {
+        Toast.makeText(this, "긴급전화 버튼이 클릭되었습니다", Toast.LENGTH_SHORT).show();
+
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        startActivity(intent);
+    }
+
+    @OnClick(R.id.fab_lockscreen_unlock)
+    public void onLockScreenUnlockClick() {
+        Toast.makeText(this, "언락 버튼이 클릭되었습니다", Toast.LENGTH_SHORT).show();
+    }
+
+    @OnClick(R.id.fab_lockscreen_mother)
+    public void onLockScreenMotherClick() {
+        Toast.makeText(this, "엄마찬스 버튼이 클릭되었습니다", Toast.LENGTH_SHORT).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,30 +69,21 @@ protected AnswerAdapter answerAdapter;
         LinearLayoutManager mLayoutManager = new LinearLayoutManager(this);
         recyclerview_lockscreen_answer.setLayoutManager(mLayoutManager);
 
+        ArrayList<String> mAnswerSet = new ArrayList<>();
+        mAnswerSet.add("답1");
+        mAnswerSet.add("답2");
+        mAnswerSet.add("답3");
+        mAnswerSet.add("답4");
+        mAnswerSet.add("답5");
+        mAnswerAdapter = new AnswerAdapter(mAnswerSet);
+
+        recyclerview_lockscreen_answer.setAdapter(mAnswerAdapter);
 
 
         setMathViewContent("<p>1,  2<sup>2</sup>  , 3, 5</p>");
     }
 
-    void setMathViewContent(String content){
+    void setMathViewContent(String content) {
         mathVebView_lockscreen.setText(content);
-    }
-
-    @OnClick(R.id.fab_lockscreen_call)
-    public void onLockScreenCallClick(){
-        Toast.makeText(this, "긴급전화 버튼이 클릭되었습니다", Toast.LENGTH_SHORT).show();
-
-        Intent intent = new Intent(Intent.ACTION_DIAL);
-        startActivity(intent);
-    }
-
-    @OnClick(R.id.fab_lockscreen_unlock)
-    public void onLockScreenUnlockClick(){
-        Toast.makeText(this, "언락 버튼이 클릭되었습니다", Toast.LENGTH_SHORT).show();
-    }
-
-    @OnClick(R.id.fab_lockscreen_mother)
-    public void onLockScreenMotherClick(){
-        Toast.makeText(this, "엄마찬스 버튼이 클릭되었습니다", Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/gbgj/gbgj/Adapter/AnswerAdapter.java
+++ app/src/main/java/com/gbgj/gbgj/Adapter/AnswerAdapter.java
@@ -1,14 +1,28 @@
 package com.gbgj.gbgj.Adapter;
 
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
+
+import com.gbgj.gbgj.R;
+
+import java.util.ArrayList;
 
 public class AnswerAdapter extends RecyclerView.Adapter<AnswerAdapter.ViewHolder>  {
     private static final String TAG = "AnswerAdapter";
 
+    private ArrayList<String> mDataSet = new ArrayList<>();
+
+    public AnswerAdapter(ArrayList<String> mDataSet) {
+        this.mDataSet = mDataSet;
+    }
+
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        private final TextView textView;
+        private final TextView textView_number;
+        private final TextView textView_answer;
 
         public ViewHolder(View v) {
             super(v);
@@ -19,11 +33,54 @@ public class AnswerAdapter extends RecyclerView.Adapter<AnswerAdapter.ViewHolder
 
                 }
             });
-            textView = (TextView) v.findViewById(R.id.textView);
+            textView_number = (TextView) v.findViewById(R.id.textview_adapter_answer_number);
+            textView_answer = (TextView) v.findViewById(R.id.textview_adapter_answer);
         }
 
-        public TextView getTextView() {
-            return textView;
+        public TextView getTextView_number() {
+            return textView_number;
+        }
+
+        public TextView getTextView_answer() {
+            return textView_answer;
         }
     }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.adapter_answer, parent, false);
+
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.getTextView_answer().setText(mDataSet.get(position));
+
+        switch (position){
+            case 0:
+                holder.getTextView_number().setText("①");
+                break;
+            case 1:
+                holder.getTextView_number().setText("②");
+                break;
+            case 2:
+                holder.getTextView_number().setText("③");
+                break;
+            case 3:
+                holder.getTextView_number().setText("④");
+                break;
+            case 4:
+                holder.getTextView_number().setText("⑤");
+                break;
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataSet.size();
+    }
+
 }
